bb9c5a36a709735df8a1eb234f1e8fca163bb5cc
JohnnyChanV <13602337187@163.com>
2025-09-23 20:57:27 -0400

    good

--- run.sh
+++ run.sh
@@ -5,25 +5,25 @@ git pull
 
 
 
-python patron_sample.py --dataset explanation_dataset --target_model Qwen2.5-3B --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 5
-python patron_sample.py --dataset explanation_dataset --target_model Qwen2.5-3B --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 10
-python patron_sample.py --dataset explanation_dataset --target_model Qwen2.5-3B --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 20
-python patron_sample.py --dataset explanation_dataset --target_model Qwen2.5-3B --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 30
+python patron_sample.py --dataset TREC --target_model Qwen2.5-3B --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 5
+python patron_sample.py --dataset TREC --target_model Qwen2.5-3B --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 10
+python patron_sample.py --dataset TREC --target_model Qwen2.5-3B --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 20
+python patron_sample.py --dataset TREC --target_model Qwen2.5-3B --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 30
 
 
-python patron_sample.py --dataset explanation_dataset --target_model Qwen2.5-7B --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 5
-python patron_sample.py --dataset explanation_dataset --target_model Qwen2.5-7B --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 10
-python patron_sample.py --dataset explanation_dataset --target_model Qwen2.5-7B --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 20
-python patron_sample.py --dataset explanation_dataset --target_model Qwen2.5-7B --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 30
+python patron_sample.py --dataset TREC --target_model Qwen2.5-7B --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 5
+python patron_sample.py --dataset TREC --target_model Qwen2.5-7B --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 10
+python patron_sample.py --dataset TREC --target_model Qwen2.5-7B --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 20
+python patron_sample.py --dataset TREC --target_model Qwen2.5-7B --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 30
 
 
-python patron_sample.py --dataset explanation_dataset --target_model llama-3b --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 5
-python patron_sample.py --dataset explanation_dataset --target_model llama-3b --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 10
-python patron_sample.py --dataset explanation_dataset --target_model llama-3b --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 20
-python patron_sample.py --dataset explanation_dataset --target_model llama-3b --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 30
+python patron_sample.py --dataset TREC --target_model Llama-3.2-3B --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 5
+python patron_sample.py --dataset TREC --target_model Llama-3.2-3B --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 10
+python patron_sample.py --dataset TREC --target_model Llama-3.2-3B --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 20
+python patron_sample.py --dataset TREC --target_model Llama-3.2-3B --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 30
 
 
-python patron_sample.py --dataset explanation_dataset --target_model llama-8b --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 5
-python patron_sample.py --dataset explanation_dataset --target_model llama-8b --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 10
-python patron_sample.py --dataset explanation_dataset --target_model llama-8b --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 20
-python patron_sample.py --dataset explanation_dataset --target_model llama-8b --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 30
+python patron_sample.py --dataset TREC --target_model Llama-3.1-8b --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 5
+python patron_sample.py --dataset TREC --target_model Llama-3.1-8b --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 10
+python patron_sample.py --dataset TREC --target_model Llama-3.1-8b --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 20
+python patron_sample.py --dataset TREC --target_model Llama-3.1-8b --k 50 --rho 0.01 --gamma 0.5 --beta 0.5 --n_sample 30
