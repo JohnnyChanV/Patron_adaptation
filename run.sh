dd31ae87382223a9914afd35a64a4acc0030c6bc
JohnnyChanV <13602337187@163.com>
2025-09-24 17:30:55 -0400

    good

--- run.sh
+++ run.sh
@@ -7,12 +7,12 @@ git pull
 #!/bin/bash
 
 models=("Qwen2.5-3B" "Qwen2.5-7B" "Llama-3.2-3B" "Llama-3.1-8B")
-samples=(5 10 20 30 100 200 300 400 500)
+samples=(100 200 300 400)
 
 for model in "${models[@]}"; do
   for n in "${samples[@]}"; do
     python patron_sample.py \
-      --dataset TREC \
+      --dataset data_proc \
       --target_model "$model" \
       --k 50 \
       --rho 0.01 \
